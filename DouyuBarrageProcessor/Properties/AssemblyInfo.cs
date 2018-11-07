8121358eb1fb17c6883181a9b1eb032a5b3ac896
JackGeminiKu <JackGeminiKu@qq.com>
2018-11-07 21:14:57 +0800

    更新版本

--- DouyuBarrageProcessor/Properties/AssemblyInfo.cs
+++ DouyuBarrageProcessor/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0919.2204")]
+[assembly: AssemblyVersion("1.7.1107.1946")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
